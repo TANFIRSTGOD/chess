5127d82a923795e162b1a0000e5265c7fc023eb2
tan05 <tan.tantham.tan@gmail.com>
2024-01-11 19:30:16 +0700

    starting the varaibles

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ class Game:
         self.big_font = pygame.font.Font("freesansbold.ttf", 50)
         self.clock = pygame.time.Clock()
 
+        # game variables and images
+        self.white_pieces = ["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook", 
+                             "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"]
+        self.white_positions = [(0,0), (1,0), (2,0), (3,0), (4,0), (5,0), (6,0), (7,0),
+                                (0,1), (1,1), (2,1), (3,1), (4,1), (5,1), (6,1), (7,1)]
+        
+        self.black_pieces = ["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook", 
+                             "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"]
+        self.white_positions = [(0,7), (1,7), (2,7), (3,7), (4,7), (5,7), (6,7), (7,7),
+                                (0,6), (1,6), (2,6), (3,6), (4,6), (5,6), (6,6), (7,6)]
+        
+        self.captured_pieces_white = []
+        self.captured_pieces_black = []
+        # 0 - whites turn, no selection: 1-whites turn, piece selected: 2- black turn no selected: 3- bkacj turn piece selected
+        self.turn_step = 0
+        self.selection = 100
+        self.valid_moves = []
+
         self.run = True
     
     def start(self):
