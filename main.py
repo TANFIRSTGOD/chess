be42e774c9dbe639579fccfd100b966dcff3eeee
tan05 <tan.tantham.tan@gmail.com>
2024-01-11 19:55:10 +0700

    pieces loaded

--- main.py
+++ main.py
@@ -24,10 +24,62 @@ class Game:
         
         self.captured_pieces_white = []
         self.captured_pieces_black = []
+        
         # 0 - whites turn, no selection: 1-whites turn, piece selected: 2- black turn no selected: 3- bkacj turn piece selected
+        
         self.turn_step = 0
         self.selection = 100
         self.valid_moves = []
+
+        #loading game pieces
+
+        self.black_queen = pygame.image.load("pieces/PNGs/No shadow/1x/b_queen_1x_ns.png")
+        self.black_queen = pygame.transform.scale(self.black_queen, (80, 80))
+        self.black_queen_small = pygame.transform.scale(self.black_queen, (80, 80))
+        
+        self.black_king = pygame.image.load("pieces/PNGs/No shadow/1x/b_king_1x_ns.png")
+        self.black_king = pygame.transform.scale(self.black_king, (80, 80))
+        self.black_king_small = pygame.transform.scale(self.black_king, (45, 45))
+
+        self.black_rook = pygame.image.load("pieces/PNGs/No shadow/1x/b_rook_1x_ns.png")
+        self.black_rook = pygame.transform.scale(self.black_rook, (80, 80))
+        self.black_rook_small = pygame.transform.scale(self.black_rook, (45, 45))
+
+        self.black_bishop = pygame.image.load("pieces/PNGs/No shadow/1x/b_bishop_1x_ns.png")
+        self.black_bishop = pygame.transform.scale(self.black_bishop, (80, 80))
+        self.black_bishop_small = pygame.transform.scale(self.black_bishop, (45, 45))
+
+        self.black_knight = pygame.image.load("pieces/PNGs/No shadow/1x/b_knight_1x_ns.png")
+        self.black_knight = pygame.transform.scale(self.black_knight, (80, 80))
+        self.black_knight_small = pygame.transform.scale(self.black_knight, (45, 45))
+
+        self.black_pawn = pygame.image.load("pieces/PNGs/No shadow/1x/b_pawn_1x_ns.png")
+        self.black_pawn = pygame.transform.scale(self.black_pawn, (65, 65))
+        self.black_pawn_small = pygame.transform.scale(self.black_pawn, (45, 45))
+
+        self.white_queen = pygame.image.load("pieces/PNGs/No shadow/1x/w_queen_1x_ns.png")
+        self.white_queen = pygame.transform.scale(self.black_queen, (80, 80))
+        self.white_queen_small = pygame.transform.scale(self.black_queen, (80, 80))
+        
+        self.white_king = pygame.image.load("pieces/PNGs/No shadow/1x/w_king_1x_ns.png")
+        self.white_king = pygame.transform.scale(self.black_king, (80, 80))
+        self.white_king_small = pygame.transform.scale(self.black_king, (45, 45))
+
+        self.white_rook = pygame.image.load("pieces/PNGs/No shadow/1x/w_rook_1x_ns.png")
+        self.white_rook = pygame.transform.scale(self.black_rook, (80, 80))
+        self.white_rook_small = pygame.transform.scale(self.black_rook, (45, 45))
+
+        self.white_bishop = pygame.image.load("pieces/PNGs/No shadow/1x/w_bishop_1x_ns.png")
+        self.white_bishop = pygame.transform.scale(self.black_bishop, (80, 80))
+        self.white_bishop_small = pygame.transform.scale(self.black_bishop, (45, 45))
+
+        self.white_knight = pygame.image.load("pieces/PNGs/No shadow/1x/w_knight_1x_ns.png")
+        self.white_knight = pygame.transform.scale(self.black_knight, (80, 80))
+        self.white_knight_small = pygame.transform.scale(self.black_knight, (45, 45))
+
+        self.white_pawn = pygame.image.load("pieces/PNGs/No shadow/1x/w_pawn_1x_ns.png")
+        self.white_pawn = pygame.transform.scale(self.black_pawn, (65, 65))
+        self.white_pawn_small = pygame.transform.scale(self.black_pawn, (45, 45))
 
         self.run = True
     
